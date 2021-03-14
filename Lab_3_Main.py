e67259119561382402e0ba287935c8501076b1f6
Artur Smiechowski <art27833@gmail.com>
2021-03-13 19:44:10 -0500

    Part 1
    Part 1 of Lab 3 near implemented

--- Lab_3_Main.py
+++ Lab_3_Main.py
@@ -1,2 +1,33 @@
+import time
+import pyautogui as pyg
 
-import pyautogui
+# %% Part 1
+
+print(pyg.position()) # Get and print current mouse pos
+# Newfile on Spyder is x=30 y=110
+# End of screen is x=2300 y=1300
+
+#pyg.click(x=30,y=110,clicks=1,interval=0.1,button='left')
+
+print(pyg.position())
+#pyg.click(button=1)
+
+# %% Part 2
+
+
+for step in range(10):
+    value = input("Please enter a string:\n")
+    cur_pos = pyg.position()
+    print(cur_pos[0])
+    print("You entered %s"%value)
+    
+    if(value == 'w'):
+        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
+    if(value == 'a'):
+        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
+    if(value == 's'):
+        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
+    if(value == 'd'):
+        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
+    if(value == 'x'):
+        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
