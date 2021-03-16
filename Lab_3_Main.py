18648684f953161db1eecbffd434390e7ef74b7d
Artur Smiechowski <art27833@gmail.com>
2021-03-16 19:15:30 -0400

    Part 2 Done
    Part 2 Loop Completed

--- Lab_3_Main.py
+++ Lab_3_Main.py
@@ -5,7 +5,12 @@ import pyautogui as pyg
 
 print(pyg.position()) # Get and print current mouse pos
 # Newfile on Spyder is x=30 y=110
-# End of screen is x=2300 y=1300
+
+# Dimensions are just screen resolution
+# Maximums x = 1920, y = 1080, ade
+x_max = 1920
+y_max = 1080
+
 
 #pyg.click(x=30,y=110,clicks=1,interval=0.1,button='left')
 
@@ -14,7 +19,6 @@ print(pyg.position())
 
 # %% Part 2
 
-
 for step in range(10):
     value = input("Please enter a string:\n")
     cur_pos = pyg.position()
@@ -22,12 +26,15 @@ for step in range(10):
     print("You entered %s"%value)
     
     if(value == 'w'):
-        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
+        pyg.moveTo(cur_pos[0],cur_pos[1]-(y_max/5))
     if(value == 'a'):
-        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
+        pyg.moveTo(cur_pos[0]-(x_max/5),cur_pos[1])
     if(value == 's'):
-        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
+        pyg.moveTo(cur_pos[0],cur_pos[1]+(y_max/5))
     if(value == 'd'):
-        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
+        pyg.moveTo(cur_pos[1]+(x_max/5),cur_pos[1])
     if(value == 'x'):
-        pyg.moveTo(cur_pos[0],cur_pos[1]-(500))
+        pyg.click()
+        
+# %% Part 3
+
