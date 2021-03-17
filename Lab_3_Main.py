28dcc80082533056afd361c060d2ab6ae5405563
Artur Smiechowski <art27833@gmail.com>
2021-03-17 13:02:50 -0400

    Load and Reshape Data
    Was I supposed to live pull data from the project file? There's no way to do that without editing the project file

--- Lab_3_Main.py
+++ Lab_3_Main.py
@@ -1,5 +1,6 @@
 import time
 import pyautogui as pyg
+import numpy as np
 
 # %% Part 1
 
@@ -18,7 +19,7 @@ print(pyg.position())
 #pyg.click(button=1)
 
 # %% Part 2
-
+'''
 for step in range(10):
     value = input("Please enter a string:\n")
     cur_pos = pyg.position()
@@ -35,6 +36,42 @@ for step in range(10):
         pyg.moveTo(cur_pos[1]+(x_max/5),cur_pos[1])
     if(value == 'x'):
         pyg.click()
-        
+'''        
 # %% Part 3
 
+# I've never imported libraries at a place other than top of script before
+# NOTE, Labs and Projects are in different working dir's, sys commands remedy that by adding those to pth at runtime
+import sys
+sys.path.insert(1, 'C:\\Users\Artur Smiechowski\Documents\BME227_Code\BME227_Project_1')
+# Import project 1 code
+import Main
+
+Main.read_and_plot_serial_data('COM4', 40, 2, 500)
+
+# %% Part 4
+
+# File names changed for convienance
+# Data pruned due to issue described in lab submission
+
+# Data File to load (L and R emg recordings)
+input_file = "Data_Values.npy"
+
+# Load Numpy Array of Sample Times (Actual, not predicted)
+emg_time = np.load("Data_Time.npy")[0:14800] # Values sliced due to signal drop
+
+# Load Numpy Array of Sample Data
+emg_voltage = np.load(input_file)[0:14800][:] # Values sliced due to signal drop
+
+# Initialize an empty 3D epoch array 
+emg_epoch = np.zeros((100, np.shape(emg_voltage)[1], int(len(emg_time)/100)+1)) 
+#(~100 is average sample count per 200ms, due to variance cannot dynamically extract)
+# Tried to avoid magic numbers where possible, epoch number +1 to account for remainder data whose index not divisible by epoch_sample_count
+
+# Fill 3D epoch array with loaded data
+for epoch in range(np.shape(emg_epoch)[2]): # Iterate Through the epochs
+    for channel in range(np.shape(emg_epoch)[1]): # Iterate Through Data Channels
+        for sample in range(np.shape(emg_epoch)[0]): # Iterate Through each sample in the epoch
+            # Set the value in emg_epoch
+            emg_epoch[sample][channel][epoch] = emg_voltage[sample*(epoch+1)][channel] # sample index +1 to prevent only pulling same data on first index
+            
+            
